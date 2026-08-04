2afbf188eb44d4c73e8886f0a0d51a44938eb419
dima22810w-ops <dima22810w@gmail.com>
2026-08-04 21:31:54 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+import os
+
+# Токен бота (будет храниться в Render)
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+# Каналы, обязательные для подписки
+CHANNELS = [
+    "@avatartgia",
+    "@avtoblogtgai",
+    "@xzrgai"
+]
+
+# Администратор бота
+ADMINS = [8507312135]
